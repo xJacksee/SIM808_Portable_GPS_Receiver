c0bdd18dcdb35a9c2f242ef69559ccbd9d2cd06a
Szymon Bartosik <szymon.bartosik75@gmail.com>
2024-08-30 17:45:58 +0200

    basic window

---
+++ code/src/SIM808_GPS_Logger.py
@@ -0,0 +1,13 @@
+import numpy as np
+import serial as s
+from tkinter import *
+from tkinter import ttk
+
+# Create GUI window
+window = Tk()
+window.title("SIM808 GPS Logger")
+window.geometry('900x900')
+
+window.mainloop()
+
+
